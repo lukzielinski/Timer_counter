5bac9f101796649240711d7eee09f5a770af36a5
ReQ <mrzielony22@gmail.com>
2023-03-08 23:52:12 +0100

    Added start stoper function

--- stoper.js
+++ stoper.js
@@ -1,6 +1,8 @@
 const start = document.getElementById('start');
 const stop = document.getElementById('stop');
 const reset = document.getElementById('reset');
+const tens = document.getElementById('tens');
+const seconds = document.getElementById('seconds');
 
 let interval;
 
@@ -11,4 +13,26 @@ start.onclick = function() {
 
 stop.onclick = function() {
     clearInterval(interval);
+}
+
+reset.onclick = function() {
+    clearInterval(interval);
+    tens.textContent = '00';
+    seconds.textContent = '00';
+    tens.innerHTML = '00';
+    seconds.innerHTML = '00';
+}
+
+function startTimer() {
+    tens.innerHTML++;
+    if(tens.innerHTML < 9){
+        tens.innerHTML = '0' + tens.innerHTML;
+    }
+    if(tens.innerHTML > 99){
+        seconds.innerHTML++;
+        tens.innerHTML = '00';
+    }
+    if(seconds.innerHTML < 9){
+        seconds.innerHTML = '0' + seconds.innerHTML;
+    }
 }
